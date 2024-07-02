f765c240caebf55861f57db4d1765fe837b55078
Alexander Harding <AHardin1@1800contacts.com>
2024-07-02 09:14:56 -0600

    Comment update

--- projects/ngx-buttercms/src/lib/constants/request-marker.ts
+++ projects/ngx-buttercms/src/lib/constants/request-marker.ts
@@ -1,7 +1,7 @@
 import { HttpContextToken } from '@angular/common/http';
 
 /**
- * @description A signifier that an Http request originated from a CMS context.
+ * @description A signifier that an Http request should be treated as if it originated from a CMS context.
  * @see {@link HttpContextToken}
  */
 export const requestMarker = new HttpContextToken<void>(() => void 0);
