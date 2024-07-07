10f960dd503e849f15717ba396e3a3b2b38107d0
Alexander Harding <AHardin1@1800contacts.com>
2024-07-07 00:11:22 -0600

    Formatting

--- projects/ngx-buttercms/src/lib/services/author.service.ts
+++ projects/ngx-buttercms/src/lib/services/author.service.ts
@@ -3,54 +3,54 @@ import { Injectable, inject } from '@angular/core';
 import { httpParameterCodec as encoder } from '../constants';
 import { Observable } from 'rxjs';
 import {
-  Author,
-  AuthorOptions,
-  Authors,
-  Response,
-  AuthorsWithRecentPosts,
+	Author,
+	AuthorOptions,
+	Authors,
+	Response,
+	AuthorsWithRecentPosts,
 } from '../types';
 import { requestMarker } from '../constants';
 
 @Injectable({ providedIn: 'root' })
 export class AuthorService {
-  private readonly http = inject(HttpClient);
+	private readonly http = inject(HttpClient);
 
-  get<Slug extends string = string>(): Observable<Response<Authors<Slug>>>;
-  get<
-    Slug extends string = string,
-    PostSlug extends string = string,
-    TagSlug extends string = string,
-    CategorySlug extends string = string
-  >(
-    options: AuthorOptions
-  ): Observable<
-    Response<AuthorsWithRecentPosts<Slug, PostSlug, TagSlug, CategorySlug>>
-  >;
-  get<Slug extends string = string>(
-    slug: Slug
-  ): Observable<Response<Author<Slug>>>;
-  get<
-    Slug extends string = string,
-    PostSlug extends string = string,
-    TagSlug extends string = string,
-    CategorySlug extends string = string
-  >(
-    slug: Slug,
-    options: AuthorOptions
-  ): Observable<
-    Response<AuthorsWithRecentPosts<Slug, PostSlug, TagSlug, CategorySlug>>
-  >;
-  get(
-    slugOrOptions?: string | AuthorOptions,
-    options?: Partial<AuthorOptions>
-  ): Observable<Response<Author | Authors>> {
-    let url = `/authors/`;
-    typeof slugOrOptions === 'string'
-      ? (url += `${slugOrOptions}/`)
-      : (options = { ...options, ...slugOrOptions });
-    return this.http.get<Response<Author | Authors>>(url, {
-      params: new HttpParams({ fromObject: options, encoder }),
-      context: new HttpContext().set(requestMarker, void 0),
-    });
-  }
+	get<Slug extends string = string>(): Observable<Response<Authors<Slug>>>;
+	get<
+		Slug extends string = string,
+		PostSlug extends string = string,
+		TagSlug extends string = string,
+		CategorySlug extends string = string,
+	>(
+		options: AuthorOptions,
+	): Observable<
+		Response<AuthorsWithRecentPosts<Slug, PostSlug, TagSlug, CategorySlug>>
+	>;
+	get<Slug extends string = string>(
+		slug: Slug,
+	): Observable<Response<Author<Slug>>>;
+	get<
+		Slug extends string = string,
+		PostSlug extends string = string,
+		TagSlug extends string = string,
+		CategorySlug extends string = string,
+	>(
+		slug: Slug,
+		options: AuthorOptions,
+	): Observable<
+		Response<AuthorsWithRecentPosts<Slug, PostSlug, TagSlug, CategorySlug>>
+	>;
+	get(
+		slugOrOptions?: string | AuthorOptions,
+		options?: Partial<AuthorOptions>,
+	): Observable<Response<Author | Authors>> {
+		let url = `/authors/`;
+		typeof slugOrOptions === 'string'
+			? (url += `${slugOrOptions}/`)
+			: (options = { ...options, ...slugOrOptions });
+		return this.http.get<Response<Author | Authors>>(url, {
+			params: new HttpParams({ fromObject: options, encoder }),
+			context: new HttpContext().set(requestMarker, void 0),
+		});
+	}
 }
--- projects/ngx-buttercms/src/lib/services/content.service.ts
+++ projects/ngx-buttercms/src/lib/services/content.service.ts
@@ -7,26 +7,26 @@ import { Content, ContentOptions, PaginatedResponse } from '../types';
 
 @Injectable({ providedIn: 'root' })
 export class ContentService {
-  private readonly http = inject(HttpClient);
+	private readonly http = inject(HttpClient);
 
-  get<
-    Fields extends Readonly<Record<keyof unknown, unknown>> = Readonly<
-      Record<keyof unknown, unknown>
-    >,
-    Type extends string = string
-  >(
-    type: Type,
-    options?: ContentOptions<Fields>
-  ): Observable<PaginatedResponse<Content<Fields, Type>>> {
-    return this.http.get<PaginatedResponse<Content<Fields, Type>>>(
-      '/content/',
-      {
-        params: new HttpParams({
-          fromObject: { ...options, keys: type },
-          encoder,
-        }),
-        context: new HttpContext().set(requestMarker, void 0),
-      }
-    );
-  }
+	get<
+		Fields extends Readonly<Record<keyof unknown, unknown>> = Readonly<
+			Record<keyof unknown, unknown>
+		>,
+		Type extends string = string,
+	>(
+		type: Type,
+		options?: ContentOptions<Fields>,
+	): Observable<PaginatedResponse<Content<Fields, Type>>> {
+		return this.http.get<PaginatedResponse<Content<Fields, Type>>>(
+			'/content/',
+			{
+				params: new HttpParams({
+					fromObject: { ...options, keys: type },
+					encoder,
+				}),
+				context: new HttpContext().set(requestMarker, void 0),
+			},
+		);
+	}
 }
