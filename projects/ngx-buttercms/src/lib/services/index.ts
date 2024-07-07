65a2d8ab94efca404ce993a3756970f86549c760
Alexander Harding <AHardin1@1800contacts.com>
2024-07-07 00:16:12 -0600

    Fix build error

--- projects/ngx-buttercms/src/lib/services/index.ts
+++ projects/ngx-buttercms/src/lib/services/index.ts
@@ -2,4 +2,3 @@ export { PageService } from './page.service';
 export { PostService } from './post.service';
 export { AuthorService } from './author.service';
 export { ContentService } from './content.service';
-export { ComponentService } from './component.service';
