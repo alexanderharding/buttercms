f4001e66f2e7602b29943f9f9fbad385ee50b545
Alexander Harding <AHardin1@1800contacts.com>
2025-01-18 15:41:40 -0700

    Comment fix

--- projects/rx-broadcast-channel/src/lib/broadcast-channel-subject-config.ts
+++ projects/rx-broadcast-channel/src/lib/broadcast-channel-subject-config.ts
@@ -17,11 +17,11 @@ export interface BroadcastChannelSubjectConfig<In = DefaultIn, Out = In> {
 const defaultTransform: TransformFn<unknown> = (event) => event.data;
 
 /** @internal */
-// This is a little hacky, but ultimately it's up to the consumer of the BroadcastChannelSubject
-// to type `In` and `Out` correctly.
 export function normalize<In = DefaultIn, Out = In>(
 	nameOrConfig: string | BroadcastChannelSubjectConfig<In, Out>,
 ): Required<BroadcastChannelSubjectConfig<In, Out>>;
+// This is a little hacky, but ultimately it's up to the consumer of the BroadcastChannelSubject
+// to type `In` and `Out` correctly.
 export function normalize(
 	nameOrConfig: string | BroadcastChannelSubjectConfig<unknown, unknown>,
 ): Required<BroadcastChannelSubjectConfig<unknown, unknown>> {
