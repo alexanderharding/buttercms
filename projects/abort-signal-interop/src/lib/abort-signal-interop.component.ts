8c1fb2ed6d75a4f4b078ada2066f132d2df800c6
Alexander Harding <AHardin1@1800contacts.com>
2025-03-07 13:48:54 -0700

    Update Angular

--- projects/abort-signal-interop/src/lib/abort-signal-interop.component.ts
+++ projects/abort-signal-interop/src/lib/abort-signal-interop.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-abort-signal-interop',
-  standalone: true,
-  imports: [],
-  template: `
+    selector: 'lib-abort-signal-interop',
+    imports: [],
+    template: `
     <p>
       abort-signal-interop works!
     </p>
   `,
-  styles: ``
+    styles: ``
 })
 export class AbortSignalInteropComponent {
 
