510d932fc636765095d18a26f891a7e37c15fdf1
Alexander Harding <AHardin1@1800contacts.com>
2025-03-22 21:27:36 -0600

    Removed unneeded code from ReplaySubject

--- projects/observable/src/lib/subject/replay-subject.ts
+++ projects/observable/src/lib/subject/replay-subject.ts
@@ -1,14 +1,7 @@
-import { Observer, Subscriber } from 'subscriber';
+import { Observer } from 'subscriber';
 import { Observable, subscribe } from '../observable/observable';
 import { Subject } from './subject';
-import { Pipeline } from '../pipe/pipeline';
-// import { of } from '../of';
-// import { defer } from '../defer';
-// import { tap } from '../tap';
-// import { from, ObservableInput, ObservedValueOf } from '../from';
-// import { connectable } from '../connect';
-// import { UnaryFunction } from '../unary-function';
-
+import { Pipeline } from '../pipe';
 /**
  * A variant of {@linkcode Subject} that emits a buffer of the last N values, or all values if less than N.
  */
@@ -52,15 +45,12 @@ export const ReplaySubject: ReplaySubjectConstructor = class<Value> {
 
 	/** @internal */
 	constructor(count = Infinity) {
-		// Initialization
 		this.#count = Math.max(1, count);
 
-		// Teardown
-		new Subscriber({
+		// Remove all references to the buffer values on finalization
+		// of this subject so they can be garbage collected.
+		this.signal.addEventListener('abort', () => (this.#buffer.length = 0), {
 			signal: this.signal,
-			// Remove all references to the buffer values on finalization
-			// of this subject so they can be garbage collected.
-			finalize: () => (this.#buffer.length = 0),
 		});
 	}
 
@@ -87,8 +77,8 @@ export const ReplaySubject: ReplaySubjectConstructor = class<Value> {
 	next(value: Value): void {
 		if (!this.signal.aborted) {
 			this.#buffer.push(value);
-			// Trim the buffer before pushing it to the value delegate so
-			// reentrant code does not get pushed values than it should.
+			// Trim the buffer before pushing it to the delegate so
+			// reentrant code does not get pushed more values than it should.
 			while (this.#buffer.length > this.#count) this.#buffer.shift();
 		}
 
@@ -110,74 +100,3 @@ export const ReplaySubject: ReplaySubjectConstructor = class<Value> {
 		return this.#output;
 	}
 };
-
-// let subject = new ReplaySubject<number>(1);
-// const privateObservable = of(1, 2, 3);
-
-// export const publicObservable = defer(() => subject).pipe(
-// 	tap({ signal: undefined, error: reset, complete: reset }),
-// );
-
-// function reset() {
-// 	subject = new ReplaySubject(1);
-// }
-
-// export function shareReplay<Input extends ObservableInput>(
-// 	count?: number,
-// ): UnaryFunction<Input, Observable<ObservedValueOf<Input>>> {
-// 	return (input) => {
-// 		let subject: ReplaySubject<ObservedValueOf<Input>>;
-// 		let active = 0;
-
-// 		return connectable(input, () => {
-// 			active++;
-// 			const isSetup = !!subject;
-// 			if (!isSetup) setup();
-// 			return subject;
-// 		}).pipe(tap({ error: reset, complete: reset, finalize }));
-
-// 		function setup() {
-// 			reset();
-// 		}
-
-// 		function reset() {
-// 			subject = new ReplaySubject(count);
-// 			from(input).subscribe(subject);
-// 		}
-
-// 		function finalize() {
-// 			if (active > 0) active--;
-// 			if (active === 0) subject = new ReplaySubject(count);
-// 		}
-// 	};
-// 	return (input: ObservableInput) => {
-// 		let subject: ReplaySubject;
-// 		const source = from(input);
-
-// 		return connectable(input, () => (subject ??= new ReplaySubject(1))).pipe(
-// 			tap({ error: setup, complete: setup }),
-// 		);
-
-// 		function setup() {
-// 			subject = new ReplaySubject(1);
-// 			source.subscribe(subject);
-// 		}
-// 	};
-// }
-
-// privateObservable.subscribe({
-// 	...subject,
-// 	// signal: undefined,
-// 	error: (error) => {
-// 		const stale = subject;
-// 		subject = new ReplaySubject(1);
-// 		stale.error(error);
-// 	},
-// 	complete: () => {
-// 		const oldSubject = subject;
-// 		subject = new ReplaySubject(1);
-// 		oldSubject.complete();
-// 	},
-// });
-
-// subject.subscribe(console.log);
