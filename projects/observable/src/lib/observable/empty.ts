c44726c5326f6d7e7f508cda1688909ab15f4061
Alexander Harding <AHardin1@1800contacts.com>
2025-03-24 00:04:54 -0600

    Polish empty and never documentation

--- projects/observable/src/lib/observable/empty.ts
+++ projects/observable/src/lib/observable/empty.ts
@@ -1,5 +1,44 @@
 import { Observable } from './observable';
 
-export const empty = new Observable<never>((subscriber) => {
-	subscriber.complete();
-});
+/**
+ * An {@linkcode Observable} that completes immediately on `subscribe` without
+ * emitting any errors or values.
+ *
+ * You can prevent the `complete` notification from being pushed if you
+ * provide an already aborted {@linkcode AbortSignal|signal} to the `Observer`.
+ * For most use-cases this is not useful, but good to know. This is unnecessary to
+ * prevent memory leaks, since the {@linkcode Observable} _never_ does any work.
+ * Regardless of the state of the {@linkcode AbortSignal|signal}, the `finalize`
+ * notification will still be pushed.
+ *
+ * @example <caption>Without Observer signal</caption>
+ * import { empty } from 'observable';
+ *
+ * empty.subscribe({
+ *  signal: undefined, // This property is unnecessary, but is explicitly defined for clarity
+ * 	next: () => console.log('Next'), // Never called
+ * 	error: () => console.log('Error'), // Never called
+ * 	complete: () => console.log('Complete'), // Called immediately
+ * 	finalize: () => console.log('Finalize'), // Called after complete
+ * });
+ * @example <caption>With Observer signal</caption>
+ * import { empty } from 'observable';
+ *
+ * const controller = new AbortController();
+ *
+ * controller.abort();
+ *
+ * empty.subscribe({
+ *  signal: controller.signal,
+ * 	next: () => console.log('Next'), // Never called
+ * 	error: () => console.log('Error'), // Never called
+ * 	complete: () => console.log('Complete'), // Never called
+ * 	finalize: () => console.log('Finalize'), // Called immediately
+ * });
+ * @see {@linkcode Observable}
+ * @constant
+ * @public
+ */
+export const empty = new Observable<never>((subscriber) =>
+	subscriber.complete(),
+);
--- projects/observable/src/lib/observable/never.ts
+++ projects/observable/src/lib/observable/never.ts
@@ -1,3 +1,40 @@
 import { Observable } from './observable';
 
+/**
+ * An {@linkcode Observable} that does no work so it _never_ pushes any `next`, `error`, or `complete` notifications.
+ *
+ * Like any other {@linkcode Observable}, you can get it to push the `finalize`
+ * notification by providing an {@linkcode AbortSignal|signal} to the Observer and
+ * aborting it before or after `subscribe`. For most use-cases this is not
+ * useful, but good to know. Keep in mind that this is unnecessary to prevent
+ * memory leaks, since the {@linkcode Observable} _never_ does any work.
+ *
+ * @example <caption>Without Observer signal</caption>
+ * import { never } from 'observable';
+ *
+ * never.subscribe({
+ *  signal: undefined, // This property is unnecessary, but is explicitly defined for clarity
+ * 	next: () => console.log('Next'), // Never called
+ * 	error: () => console.log('Error'), // Never called
+ * 	complete: () => console.log('Complete'), // Never called
+ * 	finalize: () => console.log('Finalize'), // Never called
+ * });
+ * @example <caption>With Observer signal</caption>
+ * import { never } from 'observable';
+ *
+ * const controller = new AbortController();
+ *
+ * controller.abort();
+ *
+ * never.subscribe({
+ *  signal: controller.signal, // Already aborted
+ * 	next: () => console.log('Next'), // Never called
+ * 	error: () => console.log('Error'), // Never called
+ * 	complete: () => console.log('Complete'), // Never called
+ * 	finalize: () => console.log('Finalize'), // Called immediately
+ * });
+ * @see {@linkcode Observable}
+ * @constant
+ * @public
+ */
 export const never = new Observable();
